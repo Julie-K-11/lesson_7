0b576ae107344dfb895aa6d10384fa2c58b8e07f
Julie <Julie@HOME-PC>
2024-12-06 13:15:32 +0300

    Add project files.

---
+++ Les_7/Program.cs
@@ -0,0 +1,201 @@
+﻿namespace Les_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("______________________________________1______________________________________");
+            string filePath = GetFilePath("Введіть шлях до файлу: ");
+            DisplayFileIfExists(filePath);
+            DisplayFileContent(filePath);
+
+            Console.WriteLine("______________________________________2______________________________________");
+            int[] array = GetArrayFromUser();
+            string savePath = GetFilePath("Шлях до файлу, для збереження: ");
+            SaveArrayToFile(array, savePath);
+
+            Console.WriteLine("______________________________________3______________________________________");
+            DisplayArrayFromFileIfExist();
+
+            Console.WriteLine("______________________________________4______________________________________");
+            string evenFilePath = @"C:\Users\Admin\OneDrive\Рабочий стол\even_numbers.txt";
+            string oddFilePath = @"C:\Users\Admin\OneDrive\Рабочий стол\odd_numbers.txt";
+            GenerateRandomNumbersAndSaveToFile(evenFilePath, oddFilePath);
+
+            Console.WriteLine("______________________________________5______________________________________");
+            filePath = @"C:\Users\Admin\OneDrive\Рабочий стол\example.txt";
+            DisplayFileIfExists(filePath);
+
+            string word = GetUserInput("Введіть слово для пошуку: ");
+            string fileContentText1 = File.ReadAllText(filePath).ToLower();
+            ProcessWordOccurrences(fileContentText1, word);
+
+            Console.WriteLine("______________________________________6______________________________________");
+            filePath = GetFilePath("Введіть шлях до файлу: ");
+            DisplayFileIfExists(filePath);
+
+            string fileContentText2 = File.ReadAllText(filePath);
+            ProcessTextStatistics(fileContentText2);
+
+        }
+
+        static void DisplayFileIfExists(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Файл не існує");
+            }
+        }
+
+        static void DisplayArrayFromFileIfExist()
+        {
+            string filePath = GetFilePath("Введіть шлях до файлу: ");
+            if (File.Exists(filePath))
+            {
+                DisplayArrayFromFile(filePath);
+            }
+            else
+            {
+                Console.WriteLine("Файл не існує");
+            }
+        }
+
+        static string GetFilePath(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+
+        static void DisplayArrayFromFile(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            int[] fileArray = Array.ConvertAll(lines, int.Parse);
+            Console.WriteLine("Масив:");
+            foreach (int element in fileArray)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+
+        static void DisplayFileContent(string filePath)
+        {
+            string fileContent = File.ReadAllText(filePath);
+            Console.WriteLine("Вміст Файлу:");
+            Console.WriteLine(fileContent);
+        }
+
+        static int[] GetArrayFromUser()
+        {
+            Console.Write("Введіть кількість елементів масиву: ");
+            int capacity = int.Parse(Console.ReadLine());
+            int[] array = new int[capacity];
+            for (int i = 0; i < capacity; i++)
+            {
+                Console.Write($"Введіть елемент {i + 1} масиву: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            return array;
+        }
+
+        static void SaveArrayToFile(int[] array, string filePath)
+        {
+            File.WriteAllLines(filePath, Array.ConvertAll(array, x => x.ToString()));
+        }
+
+        static void GenerateRandomNumbersAndSaveToFile(string evenFilePath, string oddFilePath)
+        {
+            Random random = new Random();
+            int[] num = Enumerable.Range(1, 10000).Select(_ => random.Next()).ToArray();
+            var evenNum = num.Where(n => n % 2 == 0).ToArray();
+            var oddNum = num.Where(n => n % 2 != 0).ToArray();
+            File.WriteAllLines(evenFilePath, Array.ConvertAll(evenNum, x => x.ToString()));
+            File.WriteAllLines(oddFilePath, Array.ConvertAll(oddNum, x => x.ToString()));
+            Console.WriteLine($"Загальна кількість чисел: {num.Length}");
+            Console.WriteLine($"Кількість парних чисел: {evenNum.Length}");
+            Console.WriteLine($"Кількість непарних чисел: {oddNum.Length}");
+        }
+
+        static string GetUserInput(string word)
+        {
+            Console.Write(word);
+            return Console.ReadLine();
+        }
+
+        static void ProcessWordOccurrences(string fileContentText, string word)
+        {
+            int wordCount = CountWord(fileContentText, word);
+            Console.WriteLine($"Кількість входжень слова '{word}': {wordCount}");
+            Console.WriteLine("Входження слова:");
+            FindWordPositions(fileContentText, word);
+
+            string reversedWord = ReverseString(word);
+            int reversedWordCount = CountWord(fileContentText, reversedWord);
+            Console.WriteLine($"Кількість входжень слова '{reversedWord}': {reversedWordCount}");
+            Console.WriteLine("Позиції входжень слова:");
+            FindWordPositions(fileContentText, reversedWord);
+        }
+
+        static void ProcessTextStatistics(string fileContentText)
+        {
+            int sentenceCount = 0;
+            int upperCaseCount = 0;
+            int lowerCaseCount = 0;
+            int vowelCount = 0;
+            int consonantCount = 0;
+            int digitCount = 0;
+
+            foreach (char c in fileContentText)
+            {
+                if (char.IsUpper(c)) upperCaseCount++;
+                else if (char.IsLower(c)) lowerCaseCount++;
+                else if (char.IsDigit(c)) digitCount++;
+
+                if ("aeiouAEIOU".Contains(c)) vowelCount++;
+                else if (char.IsLetter(c)) consonantCount++;
+
+                if (c == '.' || c == '!' || c == '?') sentenceCount++;
+            }
+
+            Console.WriteLine($"Кількість речень: {sentenceCount}");
+            Console.WriteLine($"Кількість великих літер: {upperCaseCount}");
+            Console.WriteLine($"Кількість маленьких літер: {lowerCaseCount}");
+            Console.WriteLine($"Кількість голосних літер: {vowelCount}");
+            Console.WriteLine($"Кількість приголосних літер: {consonantCount}");
+            Console.WriteLine($"Кількість цифр: {digitCount}");
+        }
+
+        static int CountWord(string text, string word)
+        {
+            int count = 0;
+            int index = 0;
+            while (index < text.Length)
+            {
+                index = text.IndexOf(word, index);
+                if (index == -1) break;
+                count++;
+                index += word.Length;
+            }
+            return count;
+        }
+
+        static void FindWordPositions(string text, string word)
+        {
+            int index = 0;
+            while (index < text.Length)
+            {
+                index = text.IndexOf(word, index);
+                if (index == -1) break;
+                Console.WriteLine($"Індекс: {index}");
+                index += word.Length;
+            }
+        }
+
+        static string ReverseString(string input)
+        {
+            char[] array = input.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+    }
+}
---
+++ Lesson.cs
@@ -0,0 +1,78 @@
+﻿//using System.Globalization;
+//using System.Security.Cryptography.X509Certificates;
+
+//namespace Les_7
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //взвємодія з файловими системами
+//            //// ? - налибл - пердбачаємо що можемо поставити null
+//            //Test(5, 7);
+//            //Test(5, null);
+//            ////Test(null, 7); // помилка
+//            //Test(8);
+//            ////string є null навіть без уточнення
+//            //Test(5, "7");
+//            //Test(5, null); //warning не помилка
+//            ////string це клас
+//            ////1-4.7 - .net - неможливо string?
+//            //------------------------------------------------------------------------
+
+//            File.WriteAllText("example.txt", "Hello, World!");
+
+//            if (File.Exists("example.txt"))
+//            {
+//                Console.WriteLine("Файл не існує");
+//                return;
+//            }
+
+//            string content = File.ReadAllText("example.txt");
+//            Console.WriteLine(content);
+
+//            File.Delete("example.txt");
+
+//            //string str = "World!";
+//            //Console.WriteLine("Hello" + str);
+//            //Console.WriteLine($"Hello + {str}");
+
+//            //------------------------------------------------------------------------
+//            //назва фалу - на робочий стіл зберегти 
+//            //абсолютний шлях
+//            // назву фалу вводить користучвач
+//            //${}
+//            //+
+
+//            //------------------------------------------------------------------------
+//            //string desktopPath = @"C:\Users\Admin\OneDrive\Рабочий стол";
+
+//            //Console.Write("Введіть назву файлу: ");
+//            //string fileName = Console.ReadLine();
+
+//            //string fullPath = $"{desktopPath}\\{fileName}.txt";
+
+//            //File.WriteAllText(fullPath, "Hello, World!");
+
+
+//        }
+
+//        //------------------------------------------------------------------------
+//        //? або null або int
+//        //public static void Test(int x, int? y = null)
+//        //public static void Test(int x, string? y)
+//        //public static void Test(int x, string y)
+//        //{
+//        //    if (y != null)
+//        //    {
+//        //        Console.WriteLine(x + y);
+//        //    }
+//        //    else
+//        //    {
+//        //        Console.WriteLine(x);
+//        //    }
+//        //}
+//        //------------------------------------------------------------------------
+
+//    }
+//}
